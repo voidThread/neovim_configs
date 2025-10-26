5fb7ee9409fa1dde7f3e5224170d86c6a0d39e7c
voidThread <dirtysqueeze@gmail.com>
2025-10-26 22:10:47 +0100

    Change lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,82 +1,109 @@
 return {
+  -- Mason: package manager for LSP/DAP/linters
   {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
+
+  -- Bridge between Mason and nvim-lspconfig
   {
     "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
     config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "bashls", "gopls", "jsonls" },
-      }
-    end
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "clangd",
+          "bashls",
+          "gopls",
+          "jsonls",
+          "neocmake",
+          "htmx",
+          "stylelint_lsp",
+          "ts_ls",
+          "marksman",
+        },
+      })
+    end,
   },
+
+  -- Core LSP configs
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup { capabilities = capabilities }
-      lspconfig.rust_analyzer.setup { capabilities = capabilities }
-      lspconfig.clangd.setup {
-        cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--completion-style=detailed",
-          "--header-insertion=iwyu",
-          "--header-insertion-decorators",
-          "--fallback-style=llvm",
-          "--query-driver=/usr/bin/gcc,/usr/bin/g++" -- add cross-compilers if you use them
-        },
-        capabilities = capabilities,
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-      }
-      lspconfig.bashls.setup { capabilities = capabilities }
-      lspconfig.gopls.setup { capabilities = capabilities }
-      lspconfig.jsonls.setup { capabilities = capabilities }
-      lspconfig.neocmake.setup { capabilities = capabilities }
-      lspconfig.htmx.setup { capabilities = capabilities }
-      lspconfig.stylelint_lsp.setup { filetypes = { "scss", "css" },
-        on_attach = function(client)
-          client.server_capabilities.document_formatting = false
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Default handler: apply to every server Mason installs
+      require("mason-lspconfig").setup_handlers({
+        function(server)
+          lspconfig[server].setup({ capabilities = capabilities })
         end,
-        capabilities = capabilities }
-      lspconfig.ts_ls.setup { capabilities = capabilities }
-      lspconfig.marksman.setup {}
 
-      -- Use LspAttach autocommand to only map the following keys
-      -- after the language server attaches to the current buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        -- Per-server tweaks
+        ["clangd"] = function()
+          lspconfig.clangd.setup({
+            cmd = {
+              "clangd",
+              "--background-index",
+              "--clang-tidy",
+              "--completion-style=detailed",
+              "--header-insertion=iwyu",
+              "--header-insertion-decorators",
+              "--fallback-style=llvm",
+              "--query-driver=/usr/bin/gcc,/usr/bin/g++",
+            },
+            capabilities = capabilities,
+            filetypes = { "c", "cpp", "objc", "objcpp" },
+          })
+        end,
+
+        ["stylelint_lsp"] = function()
+          lspconfig.stylelint_lsp.setup({
+            filetypes = { "scss", "css" },
+            capabilities = capabilities,
+            on_attach = function(client)
+              -- disable formatting if you use another formatter
+              client.server_capabilities.documentFormattingProvider = false
+            end,
+          })
+        end,
+      })
+
+      -- Extra servers that might not be managed by Mason (kept for completeness);
+      -- harmless if Mason already installed them.
+      -- lspconfig.marksman.setup({ capabilities = capabilities })
+
+      -- Keymaps after LSP attaches
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-          -- Buffer local mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
           local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-          vim.keymap.set('n', '<space>wl', function()
+
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set("n", "<space>wl", function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
           end, opts)
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
+          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<space>f", function()
+            vim.lsp.buf.format({ async = true })
           end, opts)
         end,
       })
-    end
-  }
+    end,
+  },
 }
