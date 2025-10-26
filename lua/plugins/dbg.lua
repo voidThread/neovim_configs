c8fbfa16dcdf9ef614c01394a64deb2920c13735
voidThread <dirtysqueeze@gmail.com>
2025-10-26 22:26:34 +0100

    Fix dbg config

--- lua/plugins/dbg.lua
+++ lua/plugins/dbg.lua
@@ -2,129 +2,107 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
+      "williamboman/mason.nvim",
+      "jay-babu/mason-nvim-dap.nvim", -- adapter manager
       "rcarriga/nvim-dap-ui",
       "nvim-neotest/nvim-nio",
+      "theHamsta/nvim-dap-virtual-text",
     },
     config = function()
-      local dap = require("dap")
+      -- Core mason (safe even if called elsewhere)
+      pcall(function() require("mason").setup() end)
 
-      -- Function to get the path to codelldb
-      local function get_codelldb_path()
-        local mason_registry = require("mason-registry")
-        local codelldb_pkg = mason_registry.get_package("codelldb")
-        if codelldb_pkg then
-          local codelldb_path = codelldb_pkg:get_install_path() .. "/codelldb"
-          return codelldb_path
-        else
-          print("codelldb is not installed. Please install it via Mason.")
-          return ""
+      -- mason-nvim-dap: be compatible with both old & new APIs
+      local mnd_ok, mnd = pcall(require, "mason-nvim-dap")
+      if mnd_ok then
+        mnd.setup({
+          ensure_installed = { "codelldb", "delve" },
+          automatic_installation = true,
+          -- Some versions use this:
+          automatic_setup = true,
+        })
+        if type(mnd.setup_handlers) == "function" then
+          mnd.setup_handlers() -- Newer versions
+        end
+      else
+        -- Fallback: manual adapter wire-up if mason-nvim-dap missing
+        local dap = require("dap")
+        local data = vim.fn.stdpath("data")
+        local function pick_exec(name)
+          if vim.fn.executable(name) == 1 then return name end
+          local p = data .. "/mason/bin/" .. name
+          if vim.fn.executable(p) == 1 then return p end
+          return nil
+        end
+        local codelldb = pick_exec("codelldb")
+        if codelldb then
+          dap.adapters.codelldb = {
+            type = "server",
+            port = "${port}",
+            executable = { command = codelldb, args = { "--port", "${port}" } },
+          }
         end
       end
 
-      dap.adapters.codelldb = {
-        type = 'server',
-        port = '${port}',
-        executable = {
-          command = get_codelldb_path(),
-          args = { '--port', '${port}' },
-        }
-      }
+      require("nvim-dap-virtual-text").setup()
 
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+
+      -- C / C++ / Rust configs (use codelldb adapter provided by mason-nvim-dap or fallback above)
       dap.configurations.cpp = {
         {
           name = "Launch file",
           type = "codelldb",
           request = "launch",
           program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
           end,
-          -- cwd = '${workspaceFolder}/build',
-          cwd = '${workspaceFolder}/',
+          cwd = "${workspaceFolder}",
           stopOnEntry = false,
-          terminal = 'integrated',
-          -- Arguments passed to the program (optional)
           args = function()
-            return vim.split(vim.fn.input('Arguments: '), " ")
+            local s = vim.fn.input("Arguments: ")
+            return (s == "" and {}) or vim.split(s, " ")
           end,
         },
       }
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
 
-      dap.configurations.c = {
-        {
-          name = "Launch file",
-          type = "codelldb",
-          request = "launch",
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          cwd = '${workspaceFolder}/',
-          stopOnEntry = false,
-          terminal = 'integrated',
-          -- Arguments passed to the program (optional)
-          args = {},
-        },
-      }
-
-      local dapui = require("dapui")
-
-      dapui.setup()
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-
-      vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, {})
-      vim.keymap.set('n', '<leader>dc', dap.continue, {})
-      vim.keymap.set('n', '<F10>', require 'dap'.step_over)
-      vim.keymap.set('n', '<F11>', require 'dap'.step_into)
-      vim.keymap.set('n', '<F12>', require 'dap'.step_out)
+      -- Keymaps
+      vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<leader>dc", dap.continue, {})
+      vim.keymap.set("n", "<F10>", dap.step_over, {})
+      vim.keymap.set("n", "<F11>", dap.step_into, {})
+      vim.keymap.set("n", "<F12>", dap.step_out, {})
     end,
   },
+
+  -- Go debugging (uses "delve", which mason-nvim-dap installs)
   {
     "leoluz/nvim-dap-go",
+    dependencies = { "mfussenegger/nvim-dap" },
     config = function()
-      require("dap-go").setup {
+      require("dap-go").setup({
         dap_configurations = {
           {
-            -- Must be "go" or it will be ignored by the plugin
             type = "go",
             name = "Attach remote",
             mode = "remote",
             request = "attach",
           },
         },
-        -- delve configurations
         delve = {
-          -- the path to the executable dlv which will be used for debugging.
-          -- by default, this is the "dlv" executable on your PATH.
           path = "dlv",
-          -- time to wait for delve to initialize the debug session.
-          -- default to 20 seconds
           initialize_timeout_sec = 20,
-          -- a string that defines the port to start delve debugger.
-          -- default to string "${port}" which instructs nvim-dap
-          -- to start the process in a random available port
           port = "${port}",
-          -- additional args to pass to dlv
           args = {},
-          -- the build flags that are passed to delve.
-          -- defaults to empty string, but can be used to provide flags
-          -- such as "-tags=unit" to make sure the test suite is
-          -- compiled during debugging, for example.
-          -- passing build flags using args is ineffective, as those are
-          -- ignored by delve in dap mode.
           build_flags = "",
-        }
-      }
-    end
-  }
+        },
+      })
+    end,
+  },
 }
